ece09b93939f8c2646737f7882f322b83fa3e661
Samticode <samti8686@gmail.com>
2024-01-23 15:47:47 +0100

    iopijohubh

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Samti Shekib</h2>
+            <p>IKT nerden og liker å trene</p>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Obobosbgs</p>
+        </footer>
+    );
+}
+
+export default Footer
