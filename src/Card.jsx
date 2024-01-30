e98ff78b83a30bc7c73c5a3d39610f9e398798c3
Samticode <samti8686@gmail.com>
2024-01-30 13:34:03 +0100

    Ferdig med første del av videoen (omtrent 32 minutter inn)

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,9 +2,9 @@
 function Card() {
     return (
         <div className="card">
-            <img src="" alt="" />
-            <h2>Samti Shekib</h2>
-            <p>IKT nerden og liker å trene</p>
+            <img className="card-image" src="https://avatarfiles.alphacoders.com/126/thumb-126524.png" alt="" />
+            <h2 className="card-title">Samti Shekib</h2>
+            <p className="card-text">IKT nerden og liker å trene</p>
         </div>
     );
 }
